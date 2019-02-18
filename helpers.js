8682983c6375d5d4ddbf4ca2266be34835882281
Tyler Sedlar <sedlarizona@gmail.com>
2019-02-18 11:49:27 -0700

    Create helpers.js

---
+++ helpers.js
@@ -0,0 +1,44 @@
+const createImage = (item) => {
+  const img = document.createElement('img');
+  img.src = `https://www.osrsbox.com/osrsbox-db/items-icons/${item.id}.png`;
+  img.title = `${item.id} @ ${item.time}`;
+  return img;
+}
+
+const addImages = (parent, data) => {
+  try {
+    const items = JSON.parse(data);
+    for (let item of items) {
+      parent.appendChild(createImage(item));
+    }
+  } catch (_) {
+    parent.appendChild(document.createTextNode('None!'));
+  }
+}
+
+const showItems = () => {
+  const dataEl = document.getElementById('acc-data');
+  const itemsEl = document.getElementById('items');
+  const input = dataEl.value;
+  
+  itemsEl.innerHTML = '';
+  
+  if (input.startsWith('http')) {
+    fetch(input)
+      .then(res => res.text())
+      .then(res => addImages(itemsEl, res));
+  } else {
+    addImages(itemsEl, input);
+  }
+};
+
+const params = window.location.search
+  .substring(1)
+  .split('&')
+  .map(v => v.split('='))
+  .reduce((map, [key, value]) => map.set(key, decodeURIComponent(value)), new Map()); 
+
+if (params.has('data')) {
+  document.getElementById('acc-data').value = params.get('data');
+  showItems();
+}
